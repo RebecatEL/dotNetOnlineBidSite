be3da8c339f83260f5d6c525bc17e2c440017384
Hiu <88535737+RebecatEL@users.noreply.github.com>
2023-05-19 17:00:56 -0400

    Add files via upload
    Initial upload 4

---
+++ WebProject2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,236 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading;
+using System.Threading.Tasks;
+using MailKit.Security;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+using WebProject2.Models;
+
+namespace WebProject2.Areas.Identity.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<Client> _signInManager;
+        private readonly UserManager<Client> _userManager;
+        private readonly IUserStore<Client> _userStore;
+        private readonly IUserEmailStore<Client> _emailStore;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public RegisterModel(
+            UserManager<Client> userManager,
+            IUserStore<Client> userStore,
+            SignInManager<Client> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore();
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+      
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+      
+        public string ReturnUrl { get; set; }
+
+       
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+      
+        public class InputModel
+        {
+
+            [Required]
+            [StringLength(30, ErrorMessage = "The {0} must be at {2} characters long and not longer than {1} characters", MinimumLength = 1)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(30, ErrorMessage = "The {0} must be at {2} characters long and not longer than {1} characters", MinimumLength = 1)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]    
+            [StringLength(15, ErrorMessage = "The {0} must be at {2} characters long and not longer than {1} characters", MinimumLength = 4)]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+         
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+         
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+
+        }
+
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var checkUser = await _userManager.FindByEmailAsync(Input.Email);
+                if (checkUser != null)
+                {
+                    ModelState.AddModelError("", "The email is already on use");
+                    return Page();
+                }
+
+                var checkUsername = await _userManager.FindByNameAsync(Input.UserName);
+                if (checkUsername != null)
+                {
+                    ModelState.AddModelError("", "The User Name is already on use");
+                    return Page();
+                }
+
+                var user = CreateUser();
+
+                user = new Client
+                {
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    UserName = Input.UserName,
+                    Email = Input.Email,
+                };
+
+
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user,Enum.Roles.Client.ToString());
+
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    SendEmail(userId, Input.Email, code, returnUrl);
+
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+
+        private Client CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<Client>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(Client)}'. " +
+                    $"Ensure that '{nameof(Client)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+
+        private IUserEmailStore<Client> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<Client>)_userStore;
+        }
+
+        public void SendEmail(string userId, string email, string code, string returnUrl)
+        {
+            // Construct the URL for the Verify action
+            var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Binding Webpage", "bindingcompany25@gmail.com"));
+            message.To.Add(new MailboxAddress("User", email));
+            message.Subject = "Verification Token";
+
+            message.Body = new TextPart("html")
+            {
+                Text = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>."
+
+            };
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect);
+                client.Authenticate("bindingcompany25@gmail.com", "irbsjbuhzkdouoge");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+        }
+
+
+    }
+}
---
+++ WebProject2/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using WebProject2.Models;
+
+namespace WebProject2.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ResendEmailConfirmationModel : PageModel
+    {
+        private readonly UserManager<Client> _userManager;
+        private readonly IEmailSender _emailSender;
+
+        public ResendEmailConfirmationModel(UserManager<Client> userManager, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _emailSender = emailSender;
+        }
+
+  
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+
+        public class InputModel
+        {
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+
+        public void OnGet()
+        {
+        }
+        public string EmailConfirmationUrl { get; set; }
+        public bool DisplayConfirmAccountLink { get; set; }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
+                return Page();
+            }
+
+            var user2 = await _userManager.FindByEmailAsync(Input.Email);
+            if (user2 != null)
+            {
+                user2.EmailConfirmed= true;
+                await _userManager.UpdateAsync(user2);
+                ModelState.AddModelError(string.Empty, "The user has been verified");
+                return Page();
+            }
+
+            
+            return Page();
+        }
+    }
+}
